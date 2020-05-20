960ac83e2e0b9e5930623ef1aeccf04e3ed35c43
barryydalive <barry.huang93@gmail.com>
2020-05-20 17:15:32 -0400

    sign in with google button added

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { useFirebase } from '../firebase'
+import { StyledFirebaseAuth } from 'react-firebaseui'
 
-const Login = () => (
-	<div>
-		Login
-	</div>
-)
+const Login = () => {
+	const firebase = useFirebase()
+	const uiConfig = {
+		signInFlow: 'popup',
+		signInSuccessUrl: '/app',
+		signInOptions: [firebase.googleProvider.providerId],
+	}
+
+	const signOut = async () => {
+		await firebase.auth.signOut()
+	}
+
+	return (
+		<div>
+
+			<StyledFirebaseAuth
+				uiConfig={uiConfig}
+				firebaseAuth={firebase.auth}
+			/>
+			<button onClick={signOut}> sign out</button>
+		</div>
+	)
+}
 
 export default Login
