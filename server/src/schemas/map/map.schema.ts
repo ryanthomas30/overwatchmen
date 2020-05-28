36a45998ed6d3ee1eabfdc49bb270601a14ed380
John Oncher <johnoncher@gmail.com>
2020-05-28 14:33:45 -0700

    map-schema

--- server/src/schemas/map/map.schema.ts
+++ server/src/schemas/map/map.schema.ts
@@ -1,20 +1,46 @@
-import { gql, IResolvers } from 'apollo-server'
-import { Context } from '@/context'
+import { gql, IResolvers } from "apollo-server";
+import { Context } from "@/context";
+import { dataSources } from "@/server";
 
 export const Map = gql`
-	#Operations
-	extend type Query {
-		map(mapId: ID!): Map
-	}
+  #Operations
+  extend type Query {
+    map(mapId: ID!): Map
+    maps: [Map]
+  }
 
-	#Model
-	type Map {
-		id: ID!
-		name: String!
-	}
+  extend type Mutation {
+    createMap(newMap: NewMap!): Map!
+  }
+
+  #Model
+  type Map {
+    id: ID!
+    name: String!
+    type: MapType!
+  }
+
+  input NewMap {
+    name: String!
+    type: MapType!
+  }
+
+  enum MapType {
+    control
+    escort
+    assault
+    hybrid
+  }
 `
 
 export const mapResolvers: IResolvers<any, Context> = {
-	Query: {
-	},
+  Query: {
+    map: (_, { mapId }, { dataSources }) =>
+      dataSources.mapService.getOne(mapId),
+    maps: (_, __, { dataSources }) => dataSources.mapService.getAll()
+  },
+  Mutation: {
+    createMap: (_, { newMap }, { dataSources }) =>
+      dataSources.mapService.create(newMap)
+  }
 }
