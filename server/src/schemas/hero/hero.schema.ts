b8c2b1ca0c3af8bc6c0aad0d98a2e4522e834ce0
barryydalive <barry.huang93@gmail.com>
2020-06-08 16:20:35 -0400

    Add Schema and Resolvers for Hero

--- server/src/schemas/hero/hero.schema.ts
+++ server/src/schemas/hero/hero.schema.ts
@@ -5,16 +5,34 @@ export const Hero = gql`
 	# Operations
 	extend type Query {
 		hero(heroId: ID!): Hero
+    heroes: [Hero]
 	}
+
+  extend type Mutation {
+    createHero(newHero:NewHero!): Hero!
+  }
 
 	# Model
 	type Hero {
 		id: ID!
 		name: String!
+    role: Role!
 	}
+
+  enum Role {
+    tank
+    damage
+    support
+  }
 `
 
 export const heroResolvers: IResolvers<any, Context> = {
 	Query: {
+		hero: (_, { heroId }, { dataSources }) => dataSources.heroService.getOne(heroId),
+		heroes: (_, __, { dataSources }) => dataSources.heroService.getAll(),
+	},
+	Mutation: {
+		createHero: (_, { newHero }, { dataSources }) => dataSources.heroService.create(newHero),
 	},
 }
+
--- server/src/schemas/hero/hero.service.ts
+++ server/src/schemas/hero/hero.service.ts
@@ -1,7 +1,33 @@
 import { DataSource } from 'apollo-datasource'
-// import { Hero } from './hero.entity'
+import { Hero } from './hero.entity'
 import { ProducedContext } from '@/context'
+import { Role } from '../match/match.entity'
+import { ApolloError } from 'apollo-server'
 
+interface NewHero {
+	name: string
+	role: Role
+}
 export class HeroService extends DataSource<ProducedContext> {
+	getAll() {
+		return Hero.find()
+	}
+	getOne(heroId:number) {
+		return Hero.findOne({ where: { id: heroId } })
+	}
 
+	async create(newHero:NewHero) {
+		const hero = Hero.create({
+			...newHero,
+		})
+		try {
+			const heroResponse = await hero.save()
+			return heroResponse
+		} catch (error) {
+			switch (error.code) {
+				default:
+					throw new ApolloError('An error occured when creating this hero')
+			}
+		}
+	}
 }
